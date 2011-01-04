70e7ffb02f3e9d530c7ed9ea122e38fa1831d2e6
frendhi <frendhi@frendhi-Presario.(none)>
2011-01-05 05:18:09 +0700

    conflicts

--- application/config/database.php
+++ application/config/database.php
@@ -39,7 +39,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "";
+$db['default']['password'] = "lokal789";
 $db['default']['database'] = "chibmcom_preshop";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
